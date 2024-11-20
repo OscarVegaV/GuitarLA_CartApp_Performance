1c512ddd63735b074210bc97d4042b0fd027911a
Oscar Vega <os_ar1@hotmail.com>
2024-11-20 13:34:56 -0600

    Added state and mapped guitar data in App component

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,35 @@ import { db } from "./data/db"; // Import the database of guitars
 function App() {
 
     // Initialize state with the database (db) data
-    const [data, setData] = useState([db]);// State to store guitar collection
+    const [data, setData] = useState(db);// State to store guitar collection
 
   /*   // Initialize state with the database (db) data
   // Use with: import { useEffect } from "react";
   const [data, setData] = useState([]);// State to store guitar collection
 
   useEffect (() => {
-    setData(db)
+    setData(db)); // Set data when component mounts
   }, [] )
   */
 
 
   return (
+  
     <>
     
       {/* Render the Header component */}
       <Header />  
 
       <main className="container-xl mt-5">
+
         {/* Main heading for the collection */}
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          {/* Render multiple Guitar components */}
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
+          {/* Render multiple Guitar components | Note: Only expressions are allowed inside JSX, not statements */}
+          {data.map(() => (
+            <Guitar />
+          ))}
         </div>
       </main>
 
