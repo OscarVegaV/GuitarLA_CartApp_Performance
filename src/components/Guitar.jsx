e9b2a004936acaca2968f089b5ee64f1a1e7e5e9
Oscar Vega <os_ar1@hotmail.com>
2024-11-20 20:28:18 -0600

    Add event handling for Guitar component interactions JSX

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,5 +1,13 @@
-export default function Guitar({guitar}) {
-  const { id, name, image, description, price} = guitar;
+// Guitar component: Displays individual guitar details and includes an "Add to Cart" button
+export default function Guitar({ guitar }) {
+  // Destructure the guitar object to extract properties
+  const { id, name, image, description, price } = guitar;
+
+  // Function to handle click events when the "Add to Cart" button is clicked
+  const handleClick = (id) => {
+    console.log('You clicked...', id); // Log the clicked guitar's ID
+  };
+
   
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -16,7 +24,7 @@ export default function Guitar({guitar}) {
         <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
         <p>{description}</p>
         <p className="fw-black text-primary fs-3">${price}</p>
-        <button type="button" className="btn btn-dark w-100">
+        <button type="button" className="btn btn-dark w-100" onClick={() => handleClick(guitar)} >
           Agregar al Carrito
         </button>
       </div>
